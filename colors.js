a7be5a51f2cd5a4ef82da5b2709eda62e5fdeffb
kulotskiI <ishielmishma24@gmail.com>
2024-11-04 22:00:11 +0800

    initial commit

---
+++ colors.js
@@ -0,0 +1,11 @@
+// Create an array to store colors
+let colors = ["black", "white", "gray"];
+
+// Print the initial array
+console.log("Your favorite colors are:", colors);
+
+// Add a new color to the array
+colors.push("blue");
+
+// Print the updated array
+console.log("Updated favorite colors:", colors);
